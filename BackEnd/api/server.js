6ff6031f3f0203dfa397349d65e525663352e2e6
Marcelo Alves <marcelo.junior@tcdobrasil.com.br>
2025-02-18 11:07:35 -0300

    ajustes para o deploy

--- BackEnd/api/server.js
+++ BackEnd/api/server.js
@@ -1,26 +1,39 @@
 import express from "express"
 import cors from "cors"
 import { db} from "./connect.js"
+import path from "path"
+
+const __dirname = path.resolve();
+console.log(__dirname);
+
 const app = express()
 const PORT = 3001;
 
 app.use(cors())
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
     res.send("Olá Mundo, de novo2")
 
 })
 
-app.get("/artists", async(req, res) => {
+app.get("/api/artists", async(req, res) => {
     res.send(await db.collection("artists").find({}).toArray()
 )}
 );
 
-app.get("/songs", async(req, res) => {
+app.get("/api/songs", async(req, res) => {
     res.send(await db.collection("songs").find({}).toArray()
 )});
 
+app.use(express.static( path.join(__dirname, "../../FrontEnd/dist")));
+
+app.get("*", async (req, res) => {
+    response.sendFile(path.join(__dirname,"../../FrontEnd/dist/index.html"));
+});
+
+
+
 app.listen(PORT,() =>{
-   
+    console.log("Rodando")
 
 })
